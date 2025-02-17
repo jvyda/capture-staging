d6962dc666a3c5ec6f77946384cbd9ccceb370a2
Jayant Vyda <jayant@mailgrill.com>
2025-02-17 07:02:31 +0530

    added env to next.config

--- next.config.js
+++ next.config.js
@@ -27,6 +27,11 @@ const nextConfig = {
           }
         ]
     },
+    env: {
+      AWS_REGION: process.env.AWS_REGION,
+      AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+      AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+    }
 }
 
 module.exports = nextConfig
